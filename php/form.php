861fb469a986dcfa968a97a913e944acd5a5f924
Federico Di Iorio <federico_metallica@hotmail.com>
2023-09-18 19:49:53 -0300

    Se modificaron los campos en el formulario y se creo el archivo form.php

---
+++ php/form.php
@@ -0,0 +1,8 @@
+<?php
+//Contacto mi form / name / con el server a traves de los name
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$mail = $_POST['email'];
+$fecha = $_POST['fecha'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
